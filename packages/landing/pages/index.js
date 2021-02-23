9b08e6c05ea527555fb3af84965d055bf4687ed1
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-02-23 12:17:05 -0500

    test

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -30,7 +30,6 @@ export default () => {
           <title>Rahul Portfolio</title>
           <meta name="Description" content="React next landing page" />
           <meta name="theme-color" content="#ec5555" />
-          {/* Load google fonts */}
           <link
             href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700,800|Roboto:300,400,400i,500,700,900"
             rel="stylesheet"
